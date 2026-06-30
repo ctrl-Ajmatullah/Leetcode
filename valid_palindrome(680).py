9927aa8eeb7b9bb2bbcf825fc20b5e82d7cc5652
Ajmatullah <dec0deajmat01@gmail.com>
2026-06-30 20:35:39 +0530

    add Problem-680 to Leetcode

---
+++ valid_palindrome(680).py
@@ -0,0 +1,13 @@
+"""Given a string s, return true if the s can be palindrome after deleting at most one character from it."""
+
+def validPalindrome(s):
+    l, r = 0, len(s) - 1
+    
+    while l < r:
+        if s[l] != s[r]:
+            (skipL, skipR) = (s[l+1:r+1], s[l:r])
+            return ((skipL == skipL[::-1]) or skipR == skipR[::-1])
+        (l, r) = (l + 1, r - 1)
+    return True
+
+print(validPalindrome("aab"))
