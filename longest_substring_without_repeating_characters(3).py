128fc6b3afdec001781b6d6042a0f1c3d2add125
Ajmatullah <dec0deajmat01@gmail.com>
2026-07-04 13:25:43 +0530

    add Problem-3 to Leetcode

--- longest_substring_without_repeating_characters(3).py
+++ longest_substring_without_repeating_characters(3).py
@@ -1,4 +1,16 @@
 """Given a string s, find the length of the longest substring without duplicate characters."""
 
 def lengthOfLongestSubstring(s):
+    window = set()
+    l = 0
+    longest = 0
     
+    for r in range(len(s)):
+        while s[r] in window:
+            window.remove(s[l])
+            l +=1
+        window.add(s[r])
+        longest = max(longest, len(window))
+    return longest
+
+print(lengthOfLongestSubstring("abcabcabc"))
